579717eae535b49c724b72cd58e76c98e9f09c7b
chrispo <69305550+chrispo-git@users.noreply.github.com>
2022-12-19 12:19:25 +0000

    for loops
    the `for(x Iterations){` structure has been correctly converted to `for _ in 0..x {`

--- acmd2smashscript/acmd2smashscript.py
+++ acmd2smashscript/acmd2smashscript.py
@@ -137,6 +137,7 @@ def acmd2smashscript():
             w_f = w_f.replace(")",".0)")
           w_f = w_f.replace("wait(","wait(fighter.lua_state_agent, ")
           w_f = w_f.replace("Frames=","")
+          w_f = w_f.replace("frames=","")
       if "=" in w_f:
         for i in replace:
           f = i.replace("=", "")
@@ -163,6 +164,12 @@ def acmd2smashscript():
         if len(w_f) > 1:
           if w_f[-2] == ")":
             w_f = w_f.replace("\n", ";\n")
+      if "for(" in w_f and "Iterations" in w_f:
+        iter = w_f.split("for(")
+        iterations = iter[1]
+        indent = iter[0]
+        iterations = iterations.replace(" Iterations){\n", "")
+        w_f = indent + "for _ in 0.." + iterations + " {\n"
       for x in alphabet:
         if x in w_f:
             comma = ","+x
